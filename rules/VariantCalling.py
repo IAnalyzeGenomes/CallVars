309012e2ae5e83455a29adc0ee75018064974e0f
Amit Rupani <amit4biotek@gmail.com>
2020-11-26 13:35:47 -0500

    correcting a bug in VariantCalling.py

--- rules/VariantCalling.py
+++ rules/VariantCalling.py
@@ -3,8 +3,8 @@
 rule GATK_HaplotypeCaller:
     input:
         BAM = "CallVars/BQSR/{sample}.bam",
-        COV1 = "CallVars/BQSR/{sample}_PerBaseCov.txt",
-        COV2 = "CallVars/BQSR/{sample}_PerBaseCov_LessThan20.txt"
+        # COV1 = "CallVars/BQSR/{sample}_PerBaseCov.txt",
+        # COV2 = "CallVars/BQSR/{sample}_PerBaseCov_LessThan20.txt"
     output:
         "CallVars/VCF/{sample}_germline.vcf"
     log:
@@ -33,7 +33,7 @@ rule GATK_Mutect2:
         gnomAD = expand("{REF}", REF=config["GNOMAD_Database"])
     shell:
         """
-        gatk --java-options '{params.mem}' Mutect2 -R {params.REF} -I {input.BAM} -L {params.TARGET} -tumor {wildcards.sample} --germline-resource {params.gnomAD} -O {output} &>{log}
+        gatk --java-options '{params.mem}' Mutect2 -R {params.REF} -I {input} -L {params.TARGET} -tumor {wildcards.sample} --germline-resource {params.gnomAD} -O {output} &>{log}
         #rm CallVars/TrimmedReads/{wildcards.sample}_Trimmed_R1.fastq.gz CallVars/TrimmedReads/{wildcards.sample}_Trimmed_R2.fastq.gz
         #rm CallVars/MappedReads/{wildcards.sample}.bam CallVars/SortedReads/{wildcards.sample}.bam CallVars/NoDupReads/{wildcards.sample}.bam
         """
