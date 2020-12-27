c32707749f404c3c29a6fbc64969be73bac42486
Amit Rupani <57545058+IAnalyzeGenomes@users.noreply.github.com>
2020-12-27 11:53:16 -0500

    Add files via upload

---
+++ rules/HaplotypeCaller.py
@@ -0,0 +1,16 @@
+# Rule to call germline variants using GATK HaplotypeCaller
+
+rule GATK_HaplotypeCaller:
+    input:
+        BAM = "CallVars/NoDupReads/{sample}.bam"
+    output:
+        "CallVars/VCF/{sample}_germline.vcf"
+    log:
+        "CallVars/Logs/{sample}_GATK-HaplotypeCaller.log"
+    params:
+        mem = expand("{mem}", mem=config["GATK_JAVA_config"]),
+        REF = expand("{REF}", REF=config["Reference"]),
+        dbSNP = expand("{dbSNP}", dbSNP=config["dbSNP_Database"]),
+        TARGET = expand("{TARGET}", TARGET=config["TARGET_config"])
+    shell:
+        "gatk --java-options '{params.mem}' HaplotypeCaller -R {params.REF} -I {input.BAM} --dbsnp {params.dbSNP} -L {params.TARGET} -O {output} &>{log}"
