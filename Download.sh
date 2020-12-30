38bde8eaae923eda7290fa5369042fc6f6437d09
Amit Rupani <57545058+IAnalyzeGenomes@users.noreply.github.com>
2020-12-30 11:17:41 -0500

    Update Download.sh

--- Download.sh
+++ Download.sh
@@ -7,14 +7,13 @@ wget -nc  http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa
 ./twoBitToFa hg19.2bit hg19.fa
 mv hg19.fa genome.fa
 
-## Getting human reference genome files ready for analysis
+# Getting human reference genome files ready for analysis
 samtools faidx genome.fa
-bwa index genome.fa
-gatk CreateSequenceDictionary -R genome.fa
-
+bwa index -b 50000000 genome.fa
+gatk --java-options -Xmx4g CreateSequenceDictionary -R genome.fa
 
 # Downloading and getting dbSNP files ready 
 wget -nc ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh37p13/VCF/All_20180423.vcf.gz
 zcat All_20180423.vcf.gz | parallel --pipe sed '/^#/!s/^/chr/' > dbSNP_hg19.vcf
 rm All_20180423.vcf.gz
-gatk SortVcf -I dbSNP_hg19.vcf -O dbSNP_hg19_sorted.vcf  -SD genome.dict
+gatk --java-options -Xmx4g SortVcf -I dbSNP_hg19.vcf -O dbSNP_hg19_sorted.vcf  -SD genome.dict
