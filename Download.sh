adb4d43b25f8ecee89c0593a1b3e33e9aa518150
Amit Rupani <57545058+IAnalyzeGenomes@users.noreply.github.com>
2020-12-30 01:15:03 -0500

    Sorting using GATK

--- Download.sh
+++ Download.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # Downloading human reference genome files 
+set -e
+
 wget -nc  http://hgdownload.cse.ucsc.edu/gbdb/hg19/hg19.2bit
 wget -nc  http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa 
 ./twoBitToFa hg19.2bit hg19.fa
@@ -8,45 +10,12 @@ mv hg19.fa genome.fa
 
 ## Getting human reference genome files ready for analysis
 samtools faidx genome.fa
-RESULT1=$?
-if [ $RESULT1 -eq 0 ]; then
-  echo samtools_faidx_success
-else
-  echo samtools_faidx_failed
-fi
-
 bwa index genome.fa
-RESULT2=$?
-if [ $RESULT2 -eq 0 ]; then
-  echo bwa_index_success
-else
-  echo bwa_index_failed
-fi
-
 gatk CreateSequenceDictionary -R genome.fa
-RESULT3=$?
-if [ $RESULT3 -eq 0 ]; then
-  echo gatk_CreateSequenceDictionary_success
-else
-  echo gatk_CreateSequenceDictionary_failed
-fi
+
 
 # Downloading and getting dbSNP files ready 
 wget -nc ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh37p13/VCF/All_20180423.vcf.gz
 zcat All_20180423.vcf.gz | parallel --pipe sed '/^#/!s/^/chr/' > dbSNP_hg19.vcf
-RESULT4=$?
-if [ $RESULT4 -eq 0 ]; then
-  echo dbSNP_Download_success
-else
-  echo dbSNP_Download_failed
-fi
-
 rm All_20180423.vcf.gz
-
-gatk IndexFeatureFile -F dbSNP_hg19.vcf
-RESULT5=$?
-if [ $RESULT5 -eq 0 ]; then
-  echo gatk_IndexFeatureFile_success
-else
-  echo gatk_IndexFeatureFile_failed
-fi
+gatk SortVcf -I dbSNP_hg19.vcf -O dbSNP_hg19_sorted.vcf  -SD genome.dict
