f9475a8ffa0f03b136d7d482d3ecf015897df502
Amit Rupani <amit4biotek@gmail.com>
2021-01-18 20:08:43 -0500

    StrandBiasBySample Annotation added to HaplotypeCaller

--- rules/HaplotypeCaller.py
+++ rules/HaplotypeCaller.py
@@ -5,7 +5,7 @@ rule GATK_HaplotypeCaller:
         BAM = "CallVars/NoDupReads/{sample}.bam",
         index = "CallVars/NoDupReads/{sample}.bam.bai"
     output:
-        VCF= "CallVars/VCF/{sample}_germline.vcf",
+        VCF = "CallVars/VCF/{sample}_germline.vcf",
         GATK_BAM = "CallVars/NoDupReads/{sample}_GATK.bam"
     log:
         "CallVars/Logs/{sample}_GATK-HaplotypeCaller.log"
@@ -15,4 +15,4 @@ rule GATK_HaplotypeCaller:
         dbSNP = expand("{dbSNP}", dbSNP=config["dbSNP_Database"]),
         TARGET = expand("{TARGET}", TARGET=config["TARGET_config"])
     shell:
-        "gatk --java-options '{params.mem}' HaplotypeCaller -R {params.REF} -I {input.BAM} --dbsnp {params.dbSNP} -L {params.TARGET} -O {output.VCF} --bamout {output.GATK_BAM} &>{log}"
+        "gatk --java-options '{params.mem}' HaplotypeCaller -R {params.REF} -I {input.BAM} --dbsnp {params.dbSNP} -L {params.TARGET} -A StrandBiasBySample -O {output.VCF} --bamout {output.GATK_BAM} &>{log}"
